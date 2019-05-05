8a80e40c57642ec8a8fe66a1415cfba9ef94b590
TurboLocb <turbolocb@yandex.ru>
2019-05-06 02:23:27 +0300

    hover-state-header

--- popup.js
+++ popup.js
@@ -10,9 +10,9 @@ function popup(){
     var popupButtonOpen = document.getElementById("OpenPopupButton");
     var popupButtonClose = popupForm.querySelector(".write-us-popup-close-button");
 
-    console.log(popupForm);
+    /* console.log(popupForm);
     console.log(popupButtonOpen);
-    console.log(popupButtonClose);
+    console.log(popupButtonClose); */
 
 
     popupButtonOpen.addEventListener("click", function(evt){
@@ -37,4 +37,35 @@ function popup(){
 
 }
 
+/*
+
+    Функция открывания кнопки "Найти" для input'а "Поиск по сайту"
+  
+ */
+
+function popupSearchButton(){
+
+    var searchInput = document.getElementById("headerInput");
+    var popupFormWithSearchButton = document.querySelector(".header-first-div-inner-inside-right");
+    
+    console.log(searchInput);
+    console.log(popupFormWithSearchButton);
+
+    searchInput.addEventListener("focus", function(event){
+
+        popupFormWithSearchButton.classList.remove("search-button-hide");
+
+    });
+
+    searchInput.addEventListener("blur", function(event){
+
+        popupFormWithSearchButton.classList.add("search-button-hide");
+
+    });
+
+
+
+}
+
 popup();
+popupSearchButton()
