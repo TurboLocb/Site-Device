3201f4bf1b93b292ed931aab87ade8cac9490cb1
TurboLocb <turbolocb@yandex.ru>
2019-05-10 15:13:21 +0300

    delivery-block-hover-and-focus-update

--- popup.js
+++ popup.js
@@ -41,15 +41,15 @@ function popup(){
 
     Функция открывания кнопки "Найти" для input'а "Поиск по сайту"
   
- */
+*/
 
 function popupSearchButton(){
 
     var searchInput = document.getElementById("headerInput");
     var popupFormWithSearchButton = document.querySelector(".header-first-div-inner-inside-right");
     
-    console.log(searchInput);
-    console.log(popupFormWithSearchButton);
+    /* console.log(searchInput);
+    console.log(popupFormWithSearchButton); */
 
     searchInput.addEventListener("focus", function(event){
 
@@ -62,10 +62,69 @@ function popupSearchButton(){
         popupFormWithSearchButton.classList.add("search-button-hide");
 
     });
+}
 
+function focusChangeBackgroundLine(){
+    /* var delivery = document.getElementById("delivery");    
+    var warranty = document.getElementById("warranty");
+    var credit = document.getElementById("credit"); */
+
+    var deliveryButton = document.getElementById("delivery-button");
+    var warrantyButton = document.getElementById("warranty-button");
+    var creditButton = document.getElementById("credit-button");
+
+    var deliveryBackgroundLine = document.getElementById("delivery-line");
+    var warrantyBackgroundLine = document.getElementById("warranty-line");
+    var creditBackgroundLine = document.getElementById("credit-line");
+
+    console.log(deliveryButton);
+    console.log(deliveryBackgroundLine);
+
+    /* Обрабатываем нажатие на "Доставка" */
+    deliveryButton.addEventListener("focus", function(event){
+        event.preventDefault();
+
+        console.log("Получен фокус на 'Доставке'");
+        deliveryBackgroundLine.classList.add("line-change-background-color");
+        console.log("");
+    });
+    deliveryButton.addEventListener("blur", function(event){
+        event.preventDefault();
+
+        deliveryBackgroundLine.classList.remove("line-change-background-color");
+    });
+
+    /* Обрабатываем нажатие на "Гарантия" */
+    warrantyButton.addEventListener("focus", function(event){
+        event.preventDefault();
+
+        console.log("Получен фокус на 'Доставке'");
+        warrantyBackgroundLine.classList.add("line-change-background-color");
+        console.log("");
+    });
+    warrantyButton.addEventListener("blur", function(event){
+        event.preventDefault();
+
+        warrantyBackgroundLine.classList.remove("line-change-background-color");
+    });
+
+    /* Обрабатываем нажатие на "Кредит" */
+    creditButton.addEventListener("focus", function(event){
+        event.preventDefault();
+
+        console.log("Получен фокус на 'Доставке'");
+        creditBackgroundLine.classList.add("line-change-background-color");
+        console.log("");
+    });
+    creditButton.addEventListener("blur", function(event){
+        event.preventDefault();
+
+        creditBackgroundLine.classList.remove("line-change-background-color");
+    });
 
 
 }
 
 popup();
-popupSearchButton()
+popupSearchButton();
+focusChangeBackgroundLine();
