4a3c99fe5ff3086fbea8a309af343392048d6068
Дмитрий Леонидович Михаревич <miharevich@dssa.ru>
2019-06-20 14:49:20 +0300

    +slider-update-new-jquery-script

--- js/catalog/valueSlider.js
+++ js/catalog/valueSlider.js
@@ -1,9 +1,4 @@
-function valueSlider(){
-	var slider = document.getElementById("slider-range");
-	var sliderValueMargin = slider.getElementsByTagName("span");
-	
-	// console.log(sliderValueMargin[0]);
-	// console.log(sliderValueMargin[1]);
+function valueSlider(){	
 		
 }
 valueSlider();
